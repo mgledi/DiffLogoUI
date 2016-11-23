4a37708b77bf0bf3ff9d4195c6a25212ab986ea8
Jörg Weber <weber.joerg.h@gmail.com>
2016-11-23 23:40:22 +0100

    use new line reader

--- src/server/validation/validateFile.js
+++ src/server/validation/validateFile.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-var */
 
 var logger = require('winston');
-var LineByLineReader = require('line-by-line');
+var lineReader = require('line-reader');
 var path = require('path');
 logger.level = process.env.LOG_LEVEL || 'info';
 
@@ -9,12 +9,11 @@ function validateAlignment(filePath) {
     logger.log('debug', 'Validating alignment: %s', filePath);
 
     return new Promise((resolve) => {
-        var lr = new LineByLineReader(filePath);
         var length = -1;
         var error = '';
         var row = 0;
 
-        lr.on('line', (line) => {
+        lineReader.eachLine((line, last) => {
             // in an alignment file there can be scores
             var tfbs = line.split('\t')[0];
 
@@ -26,11 +25,14 @@ function validateAlignment(filePath) {
             } else if(length !== tfbs.length) {
                 error = 'Error in line ' + row + '. All lines in ' + path.basename(filePath) + ' must have the same length!';
                 logger.log('debug', error);
+                resolve(error);
+                return false;
+            }
+
+            if (last) {
+                resolve();
             }
         });
-
-        lr.on('end', () => resolve(error));
-
     });
 }
 
@@ -38,13 +40,12 @@ function validateFasta(filePath) {
     logger.log('debug', 'Validating fasta: %s', filePath);
 
     return new Promise((resolve) => {
-        var lr = new LineByLineReader(filePath);
         var length = -1;
         var error = '';
         var row = 0;
         var tfbs = '';
 
-        lr.on('line', (line) => {
+        lineReader.eachLine((line, last) => {
             row++;
 
             if(line.trim().length === 0) {
@@ -57,6 +58,8 @@ function validateFasta(filePath) {
                     } else if(length !== tfbs.length) {
                         error = 'Error in line ' + (row - 1) + '. All lines in ' + path.basename(filePath) + ' must have the same length!';
                         logger.log('debug', error);
+                        resolve(error);
+                        return false;
                     }
                 }
 
@@ -67,8 +70,9 @@ function validateFasta(filePath) {
             }
         });
 
-        lr.on('end', () => resolve(error));
-
+        if (last) {
+            resolve();
+        }
     });
 }
 
@@ -77,38 +81,41 @@ function validatePWM(filePath) {
 
     return new Promise((resolve) => {
         var data = [];
-        var lr = new LineByLineReader(filePath);
+        var alphabetSize;
+        var motifLength;
 
-        lr.on('line', (line) => data.push(line.trim().split('\t')));
+        lineReader.eachLine.on((line, last) => {
+            data.push(line.trim().split('\t'));
 
-        lr.on('end', () => {
-            var error = '';
-            var alphabetSize = data.length;
-            var motifLength = data[0].length;
-            var m = 0, a = 0, val, sum;
+            if (last) {
+                var error = '';
+                var alphabetSize = data.length;
+                var motifLength = data[0].length;
+                var m = 0, a = 0, val, sum;
 
-            for(; m < motifLength; m++) {
-                sum = 0;
-                a = 0;
-                for(; a < alphabetSize; a++) {
-                    val = Number(data[a][m]);
-                    if(isNaN(val)) {
-                        error = 'Element ' + m + ' in line ' + a + ' is not a valid number.';
+                for(; m < motifLength; m++) {
+                    sum = 0;
+                    a = 0;
+                    for(; a < alphabetSize; a++) {
+                        val = Number(data[a][m]);
+                        if(isNaN(val)) {
+                            error = 'Element ' + m + ' in line ' + a + ' is not a valid number.';
+                            logger.log('debug', error);
+                            resolve(error);
+                        } else {
+                            sum += val;
+                        }
+                    }
+
+                    if (Math.abs(1 - sum) > 0.000001) {
+                        error = `Elements in column ${m} sum not to 1.0. (${sum})`;
                         logger.log('debug', error);
                         resolve(error);
-                    } else {
-                        sum += val;
                     }
                 }
 
-                if (Math.abs(1 - sum) > 0.000001) {
-                    error = 'Elements in column ' + m + ' sum not to 1.0. ('+sum+')';
-                    logger.log('debug', error);
-                    resolve(error);
-                }
+                resolve(error);
             }
-
-            resolve(error);
         });
     });
 }
