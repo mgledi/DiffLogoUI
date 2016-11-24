c4d4e8db92fd6741241778ef9f3b546b1a7921d7
Jörg Weber <weber.joerg.h@gmail.com>
2016-11-24 18:55:53 +0100

    * add winston as logger * delint validateFile

--- src/server/validation/validateFile.js
+++ src/server/validation/validateFile.js
@@ -1,124 +1,132 @@
 /* eslint-disable no-var */
 
- var LineByLineReader = require('line-by-line')
- var path = require('path');
+var logger = require('winston');
+var LineByLineReader = require('line-by-line');
+var path = require('path');
+logger.level = process.env.LOG_LEVEL || 'info';
 
-function validate(file) {
-
-    if (file.type === 'alignment') {
-        return validateAlignment(file.path);
-    } else if (file.type === 'fasta') {
-        return validateFasta(file.path);
-    } else if (file.type === 'pwm') {
-        return validatePWM(file.path);
-    } else {
-        return Promise.resolve('Unkown filetype. Please see help for supported file types.');
-    }
-}
-
-/**
- *
- */ 
 function validateAlignment(filePath) {
-    console.log("Validating alignment: " + filePath);
-    return new Promise((resolve, reject) => {
-        var lr = new LineByLineReader(filePath);
+    logger.log('debug', 'Validating alignment: %s', filePath);
 
+    return new Promise((resolve) => {
+        var lr = new LineByLineReader(filePath);
         var length = -1;
-        var error = "";
+        var error = '';
         var row = 0;
-        lr.on('line', function(line) {
-            row++;
+
+        lr.on('line', (line) => {
             // in an alignment file there can be scores
-            var tfbs = line.split("\t")[0];
-            if(length == -1) {
-                length = tfbs.length
-            } else if(tfbs.length == 0) {
+            var tfbs = line.split('\t')[0];
+
+            row++;
+            if(length === -1) {
+                length = tfbs.length;
+            } else if(tfbs.length === 0) {
                 // do nothing
             } else if(length !== tfbs.length) {
-                error = "Error in line " + row + ". All lines in " + path.basename(filePath) + " must have the same length!";
-                console.log(error);
+                error = 'Error in line ' + row + '. All lines in ' + path.basename(filePath) + ' must have the same length!';
+                logger.log('debug', error);
             }
         });
-        
-        lr.on("end",function (){
-            resolve(error);
-        });
 
-    }); 
+        lr.on('end', () => resolve(error));
+
+    });
 }
 
 function validateFasta(filePath) {
-    console.log("Validating fasta: " + filePath);
-    return new Promise((resolve, reject) => {
-        var lr = new LineByLineReader(filePath);
+    logger.log('debug', 'Validating fasta: %s', filePath);
 
+    return new Promise((resolve) => {
+        var lr = new LineByLineReader(filePath);
         var length = -1;
-        var error = "";
+        var error = '';
         var row = 0;
-        var tfbs = "";
-        lr.on('line', function(line) {
+        var tfbs = '';
+
+        lr.on('line', (line) => {
             row++;
-            if(line.trim().length == 0) {
+
+            if(line.trim().length === 0) {
                 // do nothing
-            } else if( line.startsWith(">") || line.startsWith(";") ) {
+            } else if (line.startsWith('>') || line.startsWith(';')) {
                 if(tfbs.length > 0) {
-                    console.log(row + ": " + tfbs + " ");
-                    if(length == -1) {
+                    logger.log('debug', '%d: %s', row, tfbs);
+
+                    if(length === -1) {
                         length = tfbs.length;
-                    }  else if(length !== tfbs.length) {
-                        error = "Error in line " + (row-1) + ". All lines in " + path.basename(filePath) + " must have the same length!";
-                        console.log(error);
+                    } else if(length !== tfbs.length) {
+                        error = 'Error in line ' + (row - 1) + '. All lines in ' + path.basename(filePath) + ' must have the same length!';
+                        logger.log('debug', error);
                     }
                 }
-                tfbs = "";
+
+                tfbs = '';
             } else {
                 // concatenate tfbs
                 tfbs += line.trim();
             }
         });
-        
-        lr.on("end",function (){
-            resolve(error);
-        });
 
-    }); 
+        lr.on('end', () => resolve(error));
+
+    });
 }
 
 function validatePWM(filePath) {
-console.log("Validating PWM: " + filePath);
-    return new Promise((resolve, reject) => {
+    logger.log('debug', 'Validating PWM: %s', filePath);
+
+    return new Promise((resolve) => {
         var data = [];
         var lr = new LineByLineReader(filePath);
-        var motifLength = -1;
-        lr.on('line', function(line) {
-            data.push(line.trim().split("\t"));
-        });
 
-        lr.on("end",function (){
-            var error = "";
+        lr.on('line', (line) => data.push(line.trim().split('\t')));
+
+        lr.on('end', () => {
+            var error = '';
             var alphabetSize = data.length;
             var motifLength = data[0].length;
-            for(var m=0; m < motifLength; m++) {
-                var sum = 0;    
-                for(var a=0; a < alphabetSize; a++) {
-                    var val = Number(data[a][m]);
+            var m = 0, a = 0, val, sum;
+
+            for(; m < motifLength; m++) {
+                sum = 0;
+
+                for(; a < alphabetSize; a++) {
+                    val = Number(data[a][m]);
+
                     if(isNaN(val)) {
-                        error = "Element " + m + " in line " + a + " is not a valid number.";
+                        error = 'Element ' + m + ' in line ' + a + ' is not a valid number.';
+                        logger.log('debug', error);
                         resolve(error);
                     } else {
                         sum += val;
                     }
-                }   
-                if( Math.abs(1 - sum) > 0.000001 ) {
-                    error = "Elements in row " + m + " sum not to 1.0.";
+                }
+
+                if (Math.abs(1 - sum) > 0.000001) {
+                    error = 'Elements in row ' + m + ' sum not to 1.0.';
+                    logger.log('debug', error);
                     resolve(error);
                 }
             }
 
             resolve(error);
         });
-    }); 
+    });
+}
+
+function validate(file) {
+
+    switch(file.type) {
+        case 'alignment':
+            return validateAlignment(file.path);
+        case 'fasta':
+            return validateAlignment(file.path);
+        case 'pwm':
+            return validateAlignment(file.path);
+        default:
+            return Promise.resolve('Unknown filetype. Please see help for supported file types');
+    }
 }
 
 module.exports = {
