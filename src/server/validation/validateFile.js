bd241e56e1b5584f486077c574ade704463b9257
Jörg Weber <weber.joerg.h@gmail.com>
2016-11-26 15:03:38 +0100

    Export validate as function instead of object

--- src/server/validation/validateFile.js
+++ src/server/validation/validateFile.js
@@ -5,6 +5,10 @@ var lineReader = require('line-reader');
 var path = require('path');
 logger.level = process.env.LOG_LEVEL || 'info';
 
+function getLineLengthError(line, filePath) {
+    return `Error in line ${line}. All lines in ${path.basename(filePath)} must have the same length!`;
+}
+
 function validateAlignment(filePath) {
     logger.log('debug', 'Validating alignment: %s', filePath);
 
@@ -13,7 +17,7 @@ function validateAlignment(filePath) {
         var error = '';
         var row = 0;
 
-        lineReader.eachLine((line, last) => {
+        lineReader.eachLine(filePath, (line, last) => {
             // in an alignment file there can be scores
             var tfbs = line.split('\t')[0];
 
@@ -23,7 +27,7 @@ function validateAlignment(filePath) {
             } else if(tfbs.length === 0) {
                 // do nothing
             } else if(length !== tfbs.length) {
-                error = 'Error in line ' + row + '. All lines in ' + path.basename(filePath) + ' must have the same length!';
+                error = getLineLengthError(row, filePath);
                 logger.log('debug', error);
                 resolve(error);
                 return false;
@@ -45,71 +49,95 @@ function validateFasta(filePath) {
         var row = 0;
         var tfbs = '';
 
-        lineReader.eachLine((line, last) => {
+        lineReader.eachLine(filePath, (line, last) => {
+            line = line.trim();
+
             row++;
 
-            if(line.trim().length === 0) {
-                // do nothing
-            } else if (line.startsWith('>') || line.startsWith(';')) {
-                if(tfbs.length > 0) {
-                    logger.log('debug', '%d: %s', row, tfbs);
-                    if(length === -1) {
-                        length = tfbs.length;
-                    } else if(length !== tfbs.length) {
-                        error = 'Error in line ' + (row - 1) + '. All lines in ' + path.basename(filePath) + ' must have the same length!';
-                        logger.log('debug', error);
-                        resolve(error);
-                        return false;
-                    }
-                }
-
-                tfbs = '';
-            } else {
-                // concatenate tfbs
-                tfbs += line.trim();
-            }
-        });
-
-        if (last) {
-            resolve();
-        }
-    });
-}
-
-function validatePWM(filePath) {
-    logger.log('debug', 'Validating PWM: %s', filePath);
-
-    return new Promise((resolve) => {
-        var data = [];
-        var alphabetSize;
-        var motifLength;
-
-        lineReader.eachLine.on((line, last) => {
-            data.push(line.trim().split('\t'));
-
-            if (last) {
-                var error = '';
-                var alphabetSize = data.length;
-                var motifLength = data[0].length;
-                var m = 0, a = 0, val, sum;
-
-                for(; m < motifLength; m++) {
-                    sum = 0;
-                    a = 0;
-                    for(; a < alphabetSize; a++) {
-                        val = Number(data[a][m]);
-                        if(isNaN(val)) {
-                            error = 'Element ' + m + ' in line ' + a + ' is not a valid number.';
+            if (line.length > 0) {
+                if (line.startsWith('>') || line.startsWith(';')) {
+                    if(tfbs.length > 0) {
+                        logger.log('debug', '%d: %s', row, tfbs);
+                        if(length === -1) {
+                            length = tfbs.length;
+                        } else if(length !== tfbs.length) {
+                            error = getLineLengthError(row - 1, filePath);
                             logger.log('debug', error);
                             resolve(error);
-                        } else {
-                            sum += val;
+                            return false;
                         }
                     }
 
-                    if (Math.abs(1 - sum) > 0.000001) {
-                        error = `Elements in column ${m} sum not to 1.0. (${sum})`;
-                        logger.log('debug', error);
+                    tfbs = '';
+                } else {
+                    // concatenate tfbs
+                    tfbs += line;
+                }
+            }
+
+            if (last) {
+                if(length !== tfbs.length) {
+                    error = getLineLengthError(row - 1, filePath);
+                    logger.log('debug', error);
+                    resolve(error);
+                }
+                resolve();
+            }
+        });
+    });
+}
+
+function getSumForPwmColumn(alphabet, columnCount) {
+    return alphabet.reduce((sum, line) => (sum + Number(line[columnCount])), 0);
+}
+
+function validatePWM(filePath) {
+    logger.log('debug', 'Validating PWM %s', filePath);
+
+    return new Promise((resolve) => {
+        var error = '';
+        var alphabet = [];
+        var lineCount = 0;
+        var lineLength = -1;
+        var columnCount = 0;
+        var totalColumns = 0;
+        var columnSum = 0;
+
+        lineReader.eachLine(filePath, (line, last) => {
+            var lineSplit = line.trim().split('\t');
+            var splitLength = lineSplit.length;
+            var splitCount = 0;
+
+            lineCount++;
+
+            if (lineLength > -1 && lineLength !== lineSplit.length) {
+                error = getLineLengthError(lineCount, filePath);
+                resolve(error);
+                return false;
+            }
+
+            for (; splitCount < splitLength; splitCount++) {
+                if (isNaN(lineSplit[splitCount])) {
+                    error = `Element ${splitCount + 1} in line ${lineCount} is not a valid number.`;
+                    break;
+                }
+            }
+
+            if (error !== '') {
+                resolve(error);
+                return false;
+            }
+
+            lineLength = lineSplit.length;
+            alphabet.push(lineSplit);
+            if (last) {
+                totalColumns = lineSplit.length;
+
+                for (; columnCount <= totalColumns; columnCount++) {
+                    columnSum = getSumForPwmColumn(alphabet, columnCount);
+                    if (Math.abs(1 - columnSum) > 0.000001) {
+                        error = `Elements in column ${columnCount + 1} sum not to 1.0 (${columnSum})`;
+                        logger.log('debug', 'validatePWM - sum error - %s', error);
                         resolve(error);
                     }
                 }
