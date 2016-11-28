75d2bd9726ecfd77e757943cc024009f8a9a8295
Jörg Weber <weber.joerg.h@gmail.com>
2016-11-28 14:12:26 +0100

    Disable redux logger in production env

--- src/ui/store.js
+++ src/ui/store.js
@@ -1,21 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
-import rootReducer from './reducers';
-
-export default (preloadedState) => {
-    const store = createStore(
-            rootReducer,
-            preloadedState,
-            applyMiddleware(thunk, createLogger())
-        );
-
-    if (module.hot) {
-        module.hot.accept('./reducers', () => {
-            const nextRootReducer = require('./reducers').default;
-            store.replaceReducer(nextRootReducer);
-        });
-    }
-
-    return store;
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./store.prod');
+} else {
+    module.exports = require('./store.dev');
+}
